fbb29b619f19ae89a985f5994040a3a2831f04f7
Alexander Schau <alexander@schau.email>
2022-03-10 15:58:28 +0000

    smaller changes

--- src/lib/Accordion.svelte
+++ src/lib/Accordion.svelte
@@ -12,6 +12,9 @@
     }}
     class="bg-gray-100 blockElem cursor-pointer"
   >
+  <svg xmlns="http://www.w3.org/2000/svg" height="1em" width="1em" fill="currentColor" class={"inline-block align-middle mr-4"+(open?" rotate-90":"")} viewBox="0 0 16 16">
+    <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
+  </svg>
     {data.name}
   </div>
   {#if open}
